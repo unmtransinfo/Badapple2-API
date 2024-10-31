490c8eb9b0f812f64520bbb8f0853d4b962441c9
Jack-42 <johnjack0987@gmail.com>
2024-10-31 11:50:41 -0600

    rm test api call

--- app/blueprints/version.py
+++ app/blueprints/version.py
@@ -5,32 +5,10 @@ from blueprints.compound_search import compound_search
 from blueprints.hiers import hiers
 from blueprints.scaffold_search import scaffold_search
 from blueprints.substance_search import substance_search
-from flasgger import swag_from
-from flask import Blueprint, current_app
+from flask import Blueprint
 
 # Set the route prefix based on version
 version = Blueprint("version", __name__, url_prefix="/api/v1")
-
-
-# Test Route
-@version.route("/")
-@swag_from(
-    {
-        "responses": {
-            200: {"description": "Returns the greeting."},
-            500: {
-                "description": "The app is misconfigured, APP_NAME config is missing or broken."
-            },
-        }
-    }
-)
-def hello():
-    """
-    Hello World!
-    Returns a greeting to the user!
-    """
-    return f'{current_app.config.get("APP_NAME")} : Hello World!'
-
 
 # Register routes
 version.register_blueprint(hiers)
