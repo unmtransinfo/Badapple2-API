80b799498e1e9d577aaf75f62992b55568a0954e
Jack-42 <johnjack0987@gmail.com>
2024-12-04 12:30:20 -0700

    add option to get scaffold id from SMILES

--- app/blueprints/scaffold_search.py
+++ app/blueprints/scaffold_search.py
@@ -15,6 +15,50 @@ scaffold_search = Blueprint("scaffold_search", __name__, url_prefix="/scaffold_s
 TAGS = ["Scaffold Search"]
 
 
+@scaffold_search.route("/get_scaffold_id", methods=["GET"])
+@swag_from(
+    {
+        "tags": TAGS,
+        "parameters": [
+            {
+                "name": "SMILES",
+                "in": "query",
+                "type": "string",
+                "required": True,
+                "description": "SMILES string representing scaffold",
+            },
+            {
+                "name": "database",
+                "in": "query",
+                "type": "str",
+                "default": ALLOWED_DB_NAMES[0],
+                "required": False,
+                "description": f"Database to fetch information from",
+                "enum": ALLOWED_DB_NAMES,
+            },
+        ],
+        "responses": {
+            200: {
+                "description": "Fetch the scaffoldID (scafid) of the given scaffold using structural search. Note that the scafid for the same scaffold can differ between databases."
+            },
+            400: {"description": "Malformed request error"},
+        },
+    }
+)
+def get_scaffold_id():
+    """
+    Return the scafid of the given scaffold, null if scaffold not found in DB.
+    """
+    scaf_smiles = request.args.get("SMILES", type=str)
+    database = get_database(request)
+    result = badapple.get_scaffold_id(scaf_smiles, database)
+    if result and len(result) > 0:
+        result = result[0]["id"]
+    else:
+        result = None
+    return jsonify(result)
+
+
 @scaffold_search.route("/get_associated_compounds", methods=["GET"])
 @swag_from(
     {
@@ -96,6 +140,7 @@ def get_associated_assay_ids():
     return jsonify(result)
 
 
+# badapple2+ only
 @scaffold_search.route("/get_active_targets", methods=["GET"])
 @swag_from(
     {
--- app/database/badapple.py
+++ app/database/badapple.py
@@ -51,12 +51,26 @@ def index_compound(cid: int, db_name: str):
 
 
 def search_scaffold(scafsmi: str, db_name: str):
+    # here we assume the given scafsmi is None if it was not a valid SMILES
+    # and that the scafsmi was canonicalized (much faster to search scafsmi than use structural search!)
     if scafsmi is None:
         return abort(400, "Invalid SMILES provided")
     query = sql.SQL("SELECT * from scaffold where scafsmi={scafsmi} LIMIT 1").format(
         scafsmi=sql.Literal(scafsmi)
     )
     return select(query, db_name)
+
+
+def get_scaffold_id(scafsmi: str, db_name: str):
+    query = sql.SQL("SELECT id FROM mols_scaf WHERE scafmol @= {scafsmi};").format(
+        scafsmi=sql.Literal(scafsmi)
+    )
+    result = None
+    try:
+        result = select(query, db_name)
+    except psycopg2.errors.DataException:
+        return abort(400, "Invalid SMILES provided")
+    return result
 
 
 def get_associated_compounds(scafid: int, db_name: str):
