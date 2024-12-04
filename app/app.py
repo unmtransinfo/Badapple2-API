90bc1de10b66edf7fd2264ae5e387541cd90d7b8
Jack-42 <johnjack0987@gmail.com>
2024-12-04 14:48:13 -0700

    update API spec

--- app/app.py
+++ app/app.py
@@ -7,16 +7,35 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-
     # Load config
     load_dotenv(".env")
     app.config.from_pyfile("config.py")
 
+    # Enhanced CORS configuration
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
+    swagger_config = {
+        "headers": [],
+        "specs": [
+            {
+                "endpoint": "apispec_1",
+                "route": "/apidocs/apispec_1.json",
+            }
+        ],
+        "static_url_path": "/apidocs/static",
+        "swagger_ui": True,
+        "specs_route": "/apidocs/",
+        "info": {
+            "title": "API for Badapple databases",
+            "description": "API which allows for programmatic access to badapple_classic and badapple2 DBs.",
+            "version": "0.0.1",
+        },
+    }
+
+    swagger = Swagger(app, config=swagger_config)
+
     # Register routes
     app.register_blueprint(version)
-
-    CORS(app)
-    swagger = Swagger(app)
 
     return app
 
