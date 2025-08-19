49a04e9e422fd2d48ba17e7028aa6639f37e1221
Jack-42 <johnjack0987@gmail.com>
2025-08-19 14:47:49 -0600

    update doc

--- app/tests/functional/test_apidocs_page.py
+++ app/tests/functional/test_apidocs_page.py
@@ -1,7 +1,7 @@
 def test_apidocs_page(test_client):
     """
-    GIVEN a Flask application configured for testing
-    WHEN the '/' page is requested (GET)
+    GIVEN a Flask/Flasger application configured for testing
+    WHEN the '/apidocs' page is requested (GET)
     THEN check that the response is valid
     """
     response = test_client.get("/apidocs/")
