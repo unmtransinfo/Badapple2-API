4039b9224a416b506898403ad37139628ed8456c
Jack-42 <johnjack0987@gmail.com>
2025-08-26 10:28:36 -0600

    add required MAX_CONTENT_LENGTH param

--- app/config.py
+++ app/config.py
@@ -8,6 +8,7 @@ APP_NAME = environ.get("APP_NAME")
 APP_PORT = environ.get("APP_PORT")
 APP_URL = environ.get("APP_URL") or "localhost"
 URL_PREFIX = environ.get("URL_PREFIX") or ""
+MAX_CONTENT_LENGTH = int(environ.get("MAX_CONTENT_LENGTH"))
 
 # Database
 databases = [
