12ed26ff5ad92b4771e7cfa18bd02cae67d2ca3f
John Ringer <jringer@chiltepin.health.unm.edu>
2025-10-20 14:30:45 -0600

    Remove debug=True

--- app/app.py
+++ app/app.py
@@ -80,4 +80,4 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=app.config.get("APP_PORT"), debug=True)
+    app.run(host="0.0.0.0", port=app.config.get("APP_PORT"))
